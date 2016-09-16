3f3d272635147494a1ec08025f30dfbf0ee262c9
sachin tyagi <sachintyagi99@gmail.com>
2016-09-16 14:11:03 +0530

    Hook_help added

--- includes/ExamSpiderDigitalClock.php
+++ includes/ExamSpiderDigitalClock.php
@@ -23,7 +23,7 @@ class ExamSpiderDigitalClock {
    */
   public function showClock($for) {
     drupal_add_js(array('getTimeLimit' => $this->getTimeLimit(), 'forForm' => $for), 'setting');
-    drupal_add_js(drupal_get_path('module', EXAM_SLIDER_EXAM_SLIDER) . '/js/exam_spider.js');
+    drupal_add_js(drupal_get_path('module', EXAM_SPIDER_EXAM_SPIDER) . '/js/exam_spider.js');
     drupal_add_js('jQuery(document).ready(function () { jQuery.countdownto(); });', array('type' => 'inline'));
   }
 
